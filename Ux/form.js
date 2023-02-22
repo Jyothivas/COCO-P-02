cebed502eb603e48bc7a8efaa09e4e3142d767d0
Subhanshu <s.pardeshi300@gmail.com>
2023-02-23 02:09:09 +0530

    new +1

--- Ux/form.js
+++ Ux/form.js
@@ -1,14 +1,12 @@
-
-
- 
 function AddRow(){
 
     let srNum =employeeData.length+1;
     var Email = document.getElementById('email').value;
     var Age = document.getElementById('age').value;
     var Gender = document.querySelector('input[name="gender"]:checked').value;
-    var Action = 'Delete'
+    var Action = '<a>Delete</a>'
 
+    
     employeeData.push([srNum,Email,Age,Gender,Action]);
 
     $('#example').on('draw.dt', function(){
@@ -33,12 +31,12 @@ function AddRow(){
     for( var i = 0; i < employeeData.length; i++){ 
     
         if ( employeeData[i][i] === empId) { 
+    
             employeeData.splice(i, 1); 
         }
-    }
-    
 
     
+    }
     console.log(employeeData)
     $('#example').on('draw.dt', function(){
     }).DataTable({
@@ -52,21 +50,3 @@ function AddRow(){
       'autoWidth': true 
     })
 } 
-
-    
-    var employeeData = [  ]	
-  var tableofEmployee=  $(document).ready(function() {
-    var tableRefresh= $('#example').DataTable( {
-	        lengthChange: false,
-          data:employeeData,
-          'destroy': true,
-          'paging': true,
-          'lengthChange': true,
-          'searching': true,
-          'ordering': true,
-          'info': true,
-          'autoWidth': true 
-	    } );
-	  
-      });      
-  
